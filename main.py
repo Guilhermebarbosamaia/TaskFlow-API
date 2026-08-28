d7ea461f9d36a1facb08b1b832fb288d1231bc54
Guilhermebarbosamaia <guilhermebarbosadesouzamartins@gmail.com>
2026-08-28 16:23:50 -0300

    Html now

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-# main.py
+
 from database import Base, engine
 from fastapi import FastAPI
 from routers import auth, tasks, users
+from fastapi.middleware.cors import CORSMiddleware
 
-# Register all database tables
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI(title="Task Management API")
+app = FastAPI(title="TaskFlow API")
 
-# Register Routers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(tasks.router)
